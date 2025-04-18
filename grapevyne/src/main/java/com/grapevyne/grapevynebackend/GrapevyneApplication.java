d39bc8cc1b397718fda7df377637b925f4233421
DaltonMo <dalton.mosley@proton.me>
2025-04-17 19:34:52 -0500

    Initial commit - Grapevyne full-stack project

---
+++ grapevyne/src/main/java/com/grapevyne/grapevynebackend/GrapevyneApplication.java
@@ -0,0 +1,13 @@
+package com.grapevyne.grapevynebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapevyneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapevyneApplication.class, args);
+    }
+
+}
---
+++ grapevyne/src/test/java/com/grapevyne/grapevynebackend/GrapevyneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grapevyne.grapevynebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapevyneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
